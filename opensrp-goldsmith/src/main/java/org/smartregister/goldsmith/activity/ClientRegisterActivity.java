4cd181bc5caf18248d9cbd8f0bf7a06c5686ba96
Allan O <a.onchuru@gmail.com>
2020-08-17 18:47:04 +0300

    :construction: Add basic project structure setup files

---
+++ opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/activity/ClientRegisterActivity.java
@@ -0,0 +1,8 @@
+package org.smartregister.goldsmith.activity;
+
+
+import org.smartregister.family.activity.BaseFamilyRegisterActivity;
+
+public class ClientRegisterActivity extends BaseFamilyRegisterActivity {
+
+}
