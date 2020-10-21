1260f991514463b88c2d88b207f36763e3a227e1
Ephraim Kigamba <ekigamba@ona.io>
2020-10-21 10:15:32 +0300

    Tag each family register row to enable clicking to profile

--- opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/configuration/AllFamiliesRegisterRowOptions.java
+++ opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/configuration/AllFamiliesRegisterRowOptions.java
@@ -1,5 +1,7 @@
 package org.smartregister.goldsmith.configuration;
 
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.view.View;
 import android.widget.TextView;
@@ -7,13 +9,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.smartregister.CoreLibrary;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.configuration.BaseRegisterRowOptions;
+import org.smartregister.family.util.Constants;
+import org.smartregister.family.util.DBConstants;
+import org.smartregister.family.util.Utils;
 import org.smartregister.goldsmith.R;
 import org.smartregister.holders.BaseRegisterViewHolder;
 import org.smartregister.view.contract.SmartRegisterClient;
 
-public class AllFamiliesRegisterRowOptions extends BaseRegisterRowOptions {
+public class AllFamiliesRegisterRowOptions extends BaseRegisterRowOptions implements View.OnClickListener {
 
     // TODO: Fix this method name to be more understandable
     @Override
@@ -34,6 +40,14 @@ public class AllFamiliesRegisterRowOptions extends BaseRegisterRowOptions {
             if (dobString != null) {
                 fillValue(viewHolder.patientNameAge, Utils.getAgeFromDate(dobString) + "");
             }*/
+
+            viewHolder.patientColumn.setTag(org.smartregister.R.id.VIEW_TYPE, RegisterViewConstants.Provider.CLIENT_COLUMN);
+            viewHolder.patientColumn.setTag(org.smartregister.R.id.VIEW_CLIENT, commonPersonObjectClient);
+            viewHolder.patientColumn.setOnClickListener(this);
+
+            viewHolder.tasksActionWrapper.setTag(org.smartregister.R.id.VIEW_TYPE, RegisterViewConstants.Provider.ACTION_BUTTON_COLUMN);
+            viewHolder.tasksActionWrapper.setTag(org.smartregister.R.id.VIEW_CLIENT, commonPersonObjectClient);
+            viewHolder.tasksActionWrapper.setOnClickListener(this);
         }
     }
 
@@ -62,6 +76,32 @@ public class AllFamiliesRegisterRowOptions extends BaseRegisterRowOptions {
     public static void fillValue(@Nullable TextView v, @NonNull String value) {
         if (v != null) {
             v.setText(value);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view != null) {
+            String viewType = (String) view.getTag(org.smartregister.R.id.VIEW_TYPE);
+            CommonPersonObjectClient patient = (CommonPersonObjectClient) view.getTag(org.smartregister.R.id.VIEW_CLIENT);
+
+            if (RegisterViewConstants.Provider.ACTION_BUTTON_COLUMN.equals(viewType)) {
+
+            } else if (RegisterViewConstants.Provider.CLIENT_COLUMN.equals(viewType)) {
+
+                Context context = CoreLibrary.getInstance().context().applicationContext();
+                Intent intent = new Intent(context, Utils.metadata().profileActivity);
+                intent.putExtra(Constants.INTENT_KEY.FAMILY_BASE_ENTITY_ID, patient.getCaseId());
+                intent.putExtra(Constants.INTENT_KEY.FAMILY_HEAD, Utils.getValue(patient.getColumnmaps(), DBConstants.KEY.FAMILY_HEAD, false));
+                intent.putExtra(Constants.INTENT_KEY.PRIMARY_CAREGIVER, Utils.getValue(patient.getColumnmaps(), DBConstants.KEY.PRIMARY_CAREGIVER, false));
+                intent.putExtra(Constants.INTENT_KEY.VILLAGE_TOWN, Utils.getValue(patient.getColumnmaps(), DBConstants.KEY.VILLAGE_TOWN, false));
+                intent.putExtra(Constants.INTENT_KEY.FAMILY_NAME, Utils.getValue(patient.getColumnmaps(), DBConstants.KEY.FIRST_NAME, false));
+                //intent.putExtra(Constants.INTENT_KEY.GO_TO_DUE_PAGE, goToDuePage);
+
+                // TODO: Finish this connection to the family profile
+                startActivity(intent);
+            }
         }
     }
 
@@ -99,4 +139,15 @@ public class AllFamiliesRegisterRowOptions extends BaseRegisterRowOptions {
         }
 
     }
+
+
+
+    public interface RegisterViewConstants {
+
+        interface Provider {
+            String CLIENT_COLUMN = "client_column";
+            String ACTION_BUTTON_COLUMN = "action_button_column";
+        }
+    }
+
 }
