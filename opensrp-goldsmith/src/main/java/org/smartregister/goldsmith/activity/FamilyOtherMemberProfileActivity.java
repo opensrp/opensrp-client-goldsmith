61c59b0aad18b9e708637cd2309d4169adb8916a
Allan O <a.onchuru@gmail.com>
2020-11-09 20:11:02 +0300

    :construction: Add start ANC registration menu option

--- opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/activity/FamilyOtherMemberProfileActivity.java
+++ opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/activity/FamilyOtherMemberProfileActivity.java
@@ -1,10 +1,17 @@
 package org.smartregister.goldsmith.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.viewpager.widget.ViewPager;
 
+import org.smartregister.AllConstants;
+import org.smartregister.chw.anc.activity.BaseAncRegisterActivity;
+import org.smartregister.chw.core.activity.CoreAncRegisterActivity;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.family.activity.BaseFamilyOtherMemberProfileActivity;
 import org.smartregister.family.adapter.ViewPagerAdapter;
 import org.smartregister.family.fragment.BaseFamilyOtherMemberProfileFragment;
@@ -13,11 +20,15 @@ import org.smartregister.family.presenter.BaseFamilyOtherMemberProfileActivityPr
 import org.smartregister.family.util.Constants;
 import org.smartregister.goldsmith.R;
 import org.smartregister.goldsmith.fragment.FamilyOtherMemberProfileFragment;
+import org.smartregister.view.activity.BaseConfigurableRegisterActivity;
 
 public class FamilyOtherMemberProfileActivity extends BaseFamilyOtherMemberProfileActivity {
+
+    private String baseEntityId;
+
     @Override
     protected void initializePresenter() {
-        String baseEntityId = getIntent().getStringExtra(Constants.INTENT_KEY.BASE_ENTITY_ID);
+        baseEntityId = getIntent().getStringExtra(Constants.INTENT_KEY.BASE_ENTITY_ID);
         String familyHead = getIntent().getStringExtra(Constants.INTENT_KEY.FAMILY_HEAD);
         String primaryCaregiver = getIntent().getStringExtra(Constants.INTENT_KEY.PRIMARY_CAREGIVER);
         String villageTown = getIntent().getStringExtra(Constants.INTENT_KEY.VILLAGE_TOWN);
@@ -45,18 +56,30 @@ public class FamilyOtherMemberProfileActivity extends BaseFamilyOtherMemberProfi
             addMember.setVisible(false);
         }
 
+        getMenuInflater().inflate(org.smartregister.chw.core.R.menu.other_member_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                break;
+        int i = item.getItemId();
+        if (i == android.R.id.home) {
+            onBackPressed();
+            return true;
+        } else if (i == R.id.action_anc_registration) {
+            startAncRegister();
+            return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void startAncRegister() {
+        Context context = this;
+        Intent intent = new Intent(context, BaseConfigurableRegisterActivity.class);
+        intent.putExtra(AllConstants.IntentExtra.MODULE_NAME, "anc");
+        intent.putExtra(AllConstants.IntentExtra.JsonForm.BASE_ENTITY_ID, baseEntityId);
+        intent.putExtra(AllConstants.IntentExtra.JsonForm.ACTION, AllConstants.IntentExtra.JsonForm.ACTION_REGISTRATION);
+
+        context.startActivity(intent);
     }
 }
