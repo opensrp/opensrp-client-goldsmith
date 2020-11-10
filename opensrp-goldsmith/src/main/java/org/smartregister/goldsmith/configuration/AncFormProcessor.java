5f3a67f35f751fb77100d7554236b4f132490980
Allan O <a.onchuru@gmail.com>
2020-11-10 17:35:22 +0300

    :construction: Implement extract ANC event client

--- opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/configuration/AncFormProcessor.java
+++ opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/configuration/AncFormProcessor.java
@@ -5,21 +5,37 @@ import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.apache.commons.lang3.tuple.Triple;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.smartregister.chw.anc.AncLibrary;
+import org.smartregister.chw.anc.util.Constants;
+import org.smartregister.chw.anc.util.JsonFormUtils;
 import org.smartregister.clientandeventmodel.Client;
 import org.smartregister.clientandeventmodel.Event;
+import org.smartregister.clientandeventmodel.EventClient;
 import org.smartregister.configuration.ModuleFormProcessor;
 import org.smartregister.domain.tag.FormTag;
+import org.smartregister.goldsmith.ChwApplication;
 import org.smartregister.goldsmith.util.SampleAppJsonFormUtils;
+import org.smartregister.repository.AllSharedPreferences;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class AncFormProcessor implements ModuleFormProcessor {
     @Override
-    public HashMap<Client, List<Event>> extractEventClient(@NonNull String s, @Nullable Intent intent, @Nullable FormTag formTag) throws JSONException {
-        return null;
+    public HashMap<Client, List<Event>> extractEventClient(@NonNull String jsonString, @Nullable Intent data, @Nullable FormTag formTag) throws JSONException {
+        AllSharedPreferences allSharedPreferences = ChwApplication.getInstance().getContext().allSharedPreferences();
+        EventClient registrationEventClient = JsonFormUtils.processRegistrationForm(allSharedPreferences, jsonString, Constants.TABLES.ANC_MEMBERS);
+
+        HashMap<Client, List<Event>> clientEventHashMap = new HashMap<>();
+        ArrayList<Event> eventList = new ArrayList<>();
+        eventList.add(registrationEventClient.getEvent());
+        clientEventHashMap.put(registrationEventClient.getClient(), eventList);
+        return clientEventHashMap;
     }
 
     @Override
