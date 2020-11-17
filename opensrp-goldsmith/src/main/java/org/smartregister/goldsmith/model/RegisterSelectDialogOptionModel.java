435d614bb12120f7383528f8fe5d402851c36f0d
Allan O <a.onchuru@gmail.com>
2020-11-17 18:36:10 +0300

    :construction: Set current module when switching registers

--- opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/model/RegisterSelectDialogOptionModel.java
+++ opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/model/RegisterSelectDialogOptionModel.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import org.smartregister.AllConstants;
+import org.smartregister.CoreLibrary;
 import org.smartregister.goldsmith.ChwApplication;
 import org.smartregister.goldsmith.domain.RegisterSelectDialogOption;
 import org.smartregister.goldsmith.util.Constants;
@@ -44,6 +45,7 @@ public class RegisterSelectDialogOptionModel implements DialogOptionModel {
 
     private void loadRegister(String moduleName) {
         Context context = ChwApplication.getInstance().getApplicationContext();
+        CoreLibrary.getInstance().setCurrentModule(moduleName);
         Intent intent = new Intent(context, BaseConfigurableRegisterActivity.class);
         intent.putExtra(AllConstants.IntentExtra.MODULE_NAME, moduleName);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
