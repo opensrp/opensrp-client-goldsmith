4e4a542dfcf04b2d5538e5620fc21d5d50cf1515
Allan O <a.onchuru@gmail.com>
2021-03-23 11:30:04 +0300

    :construction: Fix bottom nav navigation to My CHWs

--- opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/util/Utils.java
+++ opensrp-goldsmith/src/main/java/org/smartregister/goldsmith/util/Utils.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.MenuItem;
 
+import org.smartregister.AllConstants;
+import org.smartregister.CoreLibrary;
 import org.smartregister.goldsmith.R;
 import org.smartregister.goldsmith.activity.GoldsmithTaskRegisterActivity;
 import org.smartregister.goldsmith.activity.MyPerformanceActivity;
@@ -17,7 +19,10 @@ public class Utils {
         if (menuItemId == R.id.action_my_tasks) {
             intent = new Intent(activity, GoldsmithTaskRegisterActivity.class);
         } else if (menuItemId == R.id.action_my_chws) {
+            String moduleName = Constants.RegisterViewConstants.ModuleOptions.CHW;
+            CoreLibrary.getInstance().setCurrentModule(moduleName);
             intent = new Intent(activity, BaseConfigurableRegisterActivity.class);
+            intent.putExtra(AllConstants.IntentExtra.MODULE_NAME, moduleName);
         } else if (menuItemId == R.id.action_performance) {
             intent = new Intent(activity, MyPerformanceActivity.class);
         } else if (menuItemId == R.id.action_training) {
